216634dda1381e9f15f8efb05065dac9422533ee
ninewan <ninesheng@gmail.com>
2026-07-24 09:11:30 +0800

    update

--- upload.sh
+++ upload.sh
@@ -7,4 +7,5 @@ cargo build --release
 scp ./target/release/filesystem-usage-exporter  root@192.168.124.160:/home/work/filesystem-usage-exporter/
 scp ./config.yaml  root@192.168.124.160:/home/work/filesystem-usage-exporter/
 
-
+#线上重启
+ssh root@192.168.124.160 "systemctl restart filesystem-usage-exporter.service"
